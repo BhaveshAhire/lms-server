aec569173158c67ee0764862a5eeb74ebe71bc25
BhaveshAhire-aialphatech <bhaveshahire@aialphatech.com>
2025-05-25 21:03:14 +0530

    Fix CORS configuration by removing trailing slash from LMS client URL

--- app.ts
+++ app.ts
@@ -18,7 +18,7 @@ app.use(cookieParser());
 app.use(
   cors({
     credentials: true,
-    origin: ["https://lms-client-beta-kohl.vercel.app/", "http://localhost:3000"],
+    origin: ["https://lms-client-beta-kohl.vercel.app", "http://localhost:3000"],
   })
 );
 // app.use(
